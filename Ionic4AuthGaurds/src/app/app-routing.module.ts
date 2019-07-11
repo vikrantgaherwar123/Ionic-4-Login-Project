8dcae081eca3b83f5269a322ad343294f46fa978
Priyanka Datre <priyanka.datre@tacktile.in>
2019-07-11 11:20:56 +0530

    routing set to login

--- Ionic4AuthGaurds/src/app/app-routing.module.ts
+++ Ionic4AuthGaurds/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
   { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardPageModule' },
 ];
